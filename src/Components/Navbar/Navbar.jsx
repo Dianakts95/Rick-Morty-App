abee6569615491ae4f9fb6c7a1eba9e20792009e
Diana Teixeira <dianakts95@gmail.com>
2023-04-09 12:59:17 -0300

    work in progresss

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import {NavLink } from "react-router-dom";
-             // , Link
+import {NavLink , Link } from "react-router-dom";
 import  "../../App.css";
 
 
@@ -8,7 +7,9 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light bg-body-tertiary">
   <div className="container-fluid">
- 
+  <Link to="/" className="fs-3 ubuntu navbar-brand ">
+        Rick & Morty <span className="text-success">App</span>
+      </Link>
     <button 
     className="navbar-toggler" 
     type="button" 
@@ -42,7 +43,7 @@ const Navbar = () => {
 
       <div className="navbar-nav fs-5">
         
-        <NavLink activeClassName="active" to ="/" className="nav-link" >Characters</NavLink>
+        <NavLink activeclassname="active" to ="/" className="nav-link" >Characters</NavLink>
         <NavLink to ="/episodes" className="nav-link" >Episodes</NavLink>
         <NavLink to ="/location" className="nav-link" >Location</NavLink>
        
--- src/Components/Pagination/Pagination.jsx
+++ src/Components/Pagination/Pagination.jsx
@@ -1,25 +1,52 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import ReactPaginate from "react-paginate";
 
 const Pagination = ({info, pageNumber , setPageNumber}) => {
+
+  let [width , setWidth] = useState(window.innerWidth);
+  
+  let updateDimension = ()=>{
+    setWidth(window.innerWidth);
+  }
+
+  useEffect(()=> {
+    window.addEventListener("resize", updateDimension)
+    return ()=>{window.removeEventListener("resize", updateDimension)}
+  },[])
     
   return (
+    <>
+    <style jsx = "true">
+      {`
+      @media (max-width: 768px){
+        .next, .prev {display:none}
+      }
+      .pagination{
+        font-size: 14px;
+      }
+
+      `}
+    </style>
+
     <ReactPaginate 
     className="pagination justify-content-center gap-1 my-4"
     forcePage={pageNumber === 1? 0 : pageNumber -1}
     nextLabel="Next"
     previousLabel="Prev"    
-    nextClassName="btn btn-info"
-    previousClassName="btn btn-info"
+    nextClassName="btn btn-info next"
+    previousClassName="btn btn-info prev"
     pageClassName="page-item"
     pageLinkClassName="page-link"
-    activeClassName="active"
+    marginPagesDisplayed={width < 576 ? 1:2 }
+    pageRangeDisplayed={width < 576 ? 1:2 }
+    activeclassname="active"
     
     onPageChange={(data)=>{
       setPageNumber(data.selected + 1)
     }}
     pageCount={info?.pages} 
     />
+    </>
   );
 };
 // --bs-pagination-color: #0dcaf0;
